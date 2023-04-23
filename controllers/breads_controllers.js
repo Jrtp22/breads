8c3f94b42bdcb7d38801c108a6427a4b69d1bc06
jrtp22 <jrtp22@gmail.com>
2023-04-23 12:10:10 -0700

    read one

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const breads = express.Router();
-
+const Bread = require("../models/breads.js");
+//index - all
 breads.get('/', (req,res) =>{
-    res.send("This is the index at breads")
+    res.send(Bread)
+});
+//read 1
+breads.get("/:arrayIndex", (req, res)=> {
+    const arrayIndex = req.params.arrayIndex;
+    res.send(Bread[arrayIndex]);
 })
-
-module.exports = breads
+//export
+module.exports = breads;
