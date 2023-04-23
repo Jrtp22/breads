0f02a735023ebd3d7bee4a62bf8339550f69766b
jrtp22 <jrtp22@gmail.com>
2023-04-23 11:59:39 -0700

    working controller

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const breads = require('./controllers/breads_controllers');
 
 require('dotenv').config();
 const PORT = process.env.PORT;
 console.log("My port is :", PORT);
 
 const app =express();
-
+//routes
 app.get("/", (req,res) => {
     res.send("Welcome to my awesome app about bread");
 });
-
+//bread route
+const breadsControllers = require("./controllers/breads_controllers.js");
+app.use("/breads", breadsControllers);
+// listen
 app.listen(PORT, () => {
     console.log('Listening on port', PORT);
   });
